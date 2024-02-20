fb23afc7bd90131786f25e7fa3bb16a1798e8a5b
gbalissonsilva <alisson.silva@picpay.com>
2024-02-20 10:56:33 -0300

    values

---
+++ value/value.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello" + "," + " " + "World!")
+	fmt.Println("3x3=", 3*3)
+	fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
