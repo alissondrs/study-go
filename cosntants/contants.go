45bec7e2d067cc3ccdacf11cddebc8a6075aed1c
gbalissonsilva <alisson.silva@picpay.com>
2024-02-21 17:17:12 -0300

    const

---
+++ cosntants/contants.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	// "math"
+)
+
+const h string = "H-H-H"
+const o int = 100
+
+func main() {
+	fmt.Println(h)
+	const g = 500
+	fmt.Println(g)
+	const i = g /  2 
+	fmt.Println(i)
+	fmt.Println(o + i)
+	fmt.Println(int64(i))
+	fmt.Println(float64(i))
+
+	fmt.Println(math.Sin(g))
+
+
+
+
+}
