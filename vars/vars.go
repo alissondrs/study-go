11fe7b125879e5fd376c9a6ba2e3887406ae359f
gbalissonsilva <alisson.silva@picpay.com>
2024-02-21 17:16:26 -0300

    vars

---
+++ vars/vars.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hhh = "H-H-H"
+	var a, b = "hhh?", "hhh!"
+	var f = false
+	m := 1
+	k := 2
+	var n int
+	fmt.Println(hhh)
+	fmt.Println(a, b)
+	fmt.Println(a + b)
+	fmt.Println(f)
+	fmt.Println(m)
+	fmt.Println(n)
+	fmt.Println(m + k)
+}
